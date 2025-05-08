7f3114d54866320c942c0e677a6ae65451b5ea3e
bohdan-kiselov <bogdan1234242@gmail.com>
2025-05-08 15:22:55 +0300

    Додавання UNIQUE обмежень, редагування Рідмі

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ USE pharmacy;
 
 CREATE TABLE status (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    status_name VARCHAR(20) NOT NULL
+    status_name VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE users (
@@ -10,7 +10,7 @@ CREATE TABLE users (
     login VARCHAR(16) NOT NULL UNIQUE,
     email VARCHAR(254) NOT NULL UNIQUE,
     password_hash CHAR(60) NOT NULL,
-    phone_number VARCHAR(13),
+    phone_number VARCHAR(13) NOT NULL UNIQUE,
     is_verified BOOLEAN DEFAULT FALSE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     status_id INT DEFAULT 1,
@@ -18,9 +18,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE email_verification_tokens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    token CHAR(36) NOT NULL,
+    token CHAR(36) NOT NULL UNIQUE,
     expires_at DATETIME NOT NULL,
     used BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
