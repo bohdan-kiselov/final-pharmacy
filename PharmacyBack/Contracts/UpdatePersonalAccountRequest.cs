6da032b4e054d0fcd61afe21ecfb01bc03e73be3
Alex Mozheha <alekseymojega25@gmail.com>
2025-05-12 15:54:14 +0300

    FP-23: Реалізовано endpoint для редагування даних в особистому кабінеті + пароль

---
+++ PharmacyBack/Contracts/UpdatePersonalAccountRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace PharmacyBack.Contracts
+{
+    public record UpdatePersonalAccountRequest(string newLogin, string newEmail, 
+        string newPhone, string NewPass, string CurrentPassword);
+
+}
